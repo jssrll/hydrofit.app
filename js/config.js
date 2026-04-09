3e578ec4355780c46ed488d43155de2d5c499bbc
jssrll <jessrell1010@gmail.com>
2026-04-09 16:59:54 +0800

    Update

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@
 // ========================================
 
 // YOUR WORKING WEB APP URL
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzpbwNfrZ-w3CaOC4zUUw8yasNKDC4Veaz0vIO7eFY8L5dWOY3v1yhQF12BqzDPEnMSoA/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwM0dqOu6Uvi45DrDWtO8_8NeJOhVqeqWAKJcv2QYWyd3Z6uwEPzBZCitVyYblTsJfTXQ/exec";
 
 console.log("✅ HydroFit Config Loaded");
