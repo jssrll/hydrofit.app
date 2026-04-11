d0f81c36bab1e219feb14dd03efc177dd42fcac6
jssrll <jessrell1010@gmail.com>
2026-04-11 20:23:31 +0800

    Update

--- js/app.js
+++ js/app.js
@@ -32,7 +32,6 @@ function switchTab(tab) {
     'recovery': () => { updatePageTitle('Recovery Tracker'); renderRecoveryTracker(); window.isLoading = false; },
     'bodytype': () => { updatePageTitle('Body Type'); renderBodyType(); window.isLoading = false; },
     'bodyparts': () => { updatePageTitle('Body Parts Trainer'); renderBodyParts(); window.isLoading = false; },
-    'download': () => { updatePageTitle('Download HydroFit'); renderDownload(); window.isLoading = false; },
     'warmup': () => { updatePageTitle('Warm-up & Cooldown'); renderWarmup(); window.isLoading = false; },
     'scheduler': () => { updatePageTitle('Workout Scheduler'); renderScheduler(); window.isLoading = false; },
     'telegram': () => { updatePageTitle('Community Chat'); renderTelegramCommunity(); window.isLoading = false; },
