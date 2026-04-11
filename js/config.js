ff4dd923f24316f8663cc2993145dc6cd9cc2506
jssrll <jessrell1010@gmail.com>
2026-04-11 13:17:15 +0800

    Update

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@
 // HYDROFIT - CONFIGURATION
 // ========================================
 
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzgl22R_w7UVnt9VauGVxwcrbSNUWGKTDfpbrBxnaIgGKU8Txuy49i-aI9IyFVLHHs/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzy1jhXRoNgvnard41k8B7VLiA7yOF-l621Qq2SbAQAR_HSajx4QFmf6vfRdbaXEkr8Eg/exec";
 
 console.log("✅ HydroFit Config Loaded");
